ebb41a2c6e0a794cc6c03f4091e3fbd4ca22e439
LukHet <lukaszhetman1@gmail.com>
2023-07-27 02:16:46 +0200

    Added functionality and styling for air pollution data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [weatherData, setWeatherData] = useState({});
+  const [lattitude, setLattitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [airData, setAirData] = useState({});
   const [city, setCity] = useState("");
 
   const token = "e0784f1b0eeae9eab1bf489ae9020501";
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&units=metric`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&units=metric`;
+
+  const airUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lattitude}&lon=${longitude}&appid=${token}`;
 
   const searchCity = (e) => {
     if (e.key === "Enter") {
       axios
-        .get(url)
+        .get(weatherUrl)
         .then((response) => {
-          setData(response.data);
-          console.log(response.data);
+          setWeatherData(response.data);
+          setLattitude(response.data.coord.lat);
+          setLongitude(response.data.coord.lon);
         })
         .catch((error) => console.log(error));
     }
   };
+
+  useEffect(() => {
+    if (weatherData.main) {
+      axios
+        .get(airUrl)
+        .then((response) => {
+          setAirData(response.data.list[0].components);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [weatherData, airUrl]);
 
   const handleChange = (e) => {
     setCity(e.target.value);
@@ -37,35 +54,79 @@ const App = () => {
           placeholder="Type your city..."
         />
         <div className="top">
-          {data.main ? (
+          {weatherData.main ? (
             <>
-              <div className="cityname">{data.name}</div>
+              <div className="cityname">{weatherData.name}</div>
               <div className="temperature">
-                {data.main.temp.toFixed()}&deg;C
+                {weatherData.main.temp.toFixed()}&deg;C
               </div>
               <div className="weather">
-                <div className="description">{data.weather[0].description}</div>
+                <div className="description">
+                  {weatherData.weather[0].description}
+                </div>
                 <img
                   alt=""
-                  src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
                   className="icon"
                 ></img>
               </div>
             </>
           ) : null}
         </div>
-        <div className="bottom">
-          {data.main ? (
+        <div className="middle">
+          {weatherData.main ? (
             <>
               <div className="humidity">
-                Humidity <div className="value">{data.main.humidity}%</div>
+                Humidity
+                <div className="value">{weatherData.main.humidity}%</div>
               </div>
               <div className="pressure">
-                Pressure <div className="value">{data.main.pressure}hPa</div>
+                Pressure
+                <div className="value">{weatherData.main.pressure}hPa</div>
               </div>
               <div className="wind">
                 Wind speed
-                <div className="value">{data.wind.speed.toFixed()} m/s</div>
+                <div className="value">
+                  {weatherData.wind.speed.toFixed()} m/s
+                </div>
+              </div>
+            </>
+          ) : null}
+        </div>
+        <div className="bottom">
+          {airData.co ? (
+            <>
+              <div className="co">
+                CO
+                <div className="value">{airData.co}</div>
+              </div>
+              <div className="no">
+                NO
+                <div className="value">{airData.no}</div>
+              </div>
+              <div className="NO2">
+                NO2
+                <div className="value">{airData.no2}</div>
+              </div>
+              <div className="O3">
+                O3
+                <div className="value">{airData.o3}</div>
+              </div>
+              <div className="SO2">
+                SO2
+                <div className="value">{airData.so2}</div>
+              </div>
+              <div className="PM2_5">
+                PM2_5
+                <div className="value">{airData.pm2_5}</div>
+              </div>
+              <div className="PM10">
+                PM10
+                <div className="value">{airData.pm10}</div>
+              </div>
+              <div className="NH3">
+                NH3
+                <div className="value">{airData.nh3}</div>
               </div>
             </>
           ) : null}
